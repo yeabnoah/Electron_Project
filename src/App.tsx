0a2ae11dd684c0ee66dbd28b23558b346bcb83f7
Technerd345 <yeabnoah5@gmail.com>
2024-04-03 11:52:36 +0300

    adding the first mock files to the repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,104 @@
-import { useState } from 'react'
-import UpdateElectron from '@/components/update'
-import logoVite from './assets/logo-vite.svg'
-import logoElectron from './assets/logo-electron.svg'
-import './App.css'
+import { useState } from "react";
+import UpdateElectron from "@/components/update";
+import logoVite from "./assets/logo-vite.svg";
+import logoElectron from "./assets/logo-electron.svg";
+import "./App.css";
+import headset from "./assets/test.png";
+import {
+  FaArchive,
+  FaBeer,
+  FaHeart,
+  FaHeartbeat,
+  FaHistory,
+  FaList,
+  FaPlay,
+} from "react-icons/fa";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   return (
-    <div className='App'>
-      <div className='logo-box'>
-        <a href='https://github.com/electron-vite/electron-vite-react' target='_blank'>
-          <img src={logoVite} className='logo vite' alt='Electron + Vite logo' />
-          <img src={logoElectron} className='logo electron' alt='Electron + Vite logo' />
-        </a>
-      </div>
-      <h1>Electron + Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Electron + Vite logo to learn more
-      </p>
-      <div className='flex-center'>
-        Place static files into the<code>/public</code> folder <img style={{ width: '5em' }} src='./node.svg' alt='Node logo' />
-      </div>
+    <div
+      className=" flex-1 bg-red-200 flex gap-1 home bg-cover"
+      // style={{ backgroundImage: `url("./assets/app-bg.jpg")` }}
+    >
+      <div
+        className=" flex-row justify-center items-center h-[94vh] my-[3vh] ml-5  rounded-md shadow-inherit "
+        style={{ flex: 1, backgroundColor: "rgba(255,255,255,0.18)" }}
+      >
+        <div className=" flex-row justify-center items-center">
+          <div className=" flex justify-center items-center">
+            <img
+              src={headset}
+              alt="logo-vite"
+              className=" h-52 w-52 border-2 "
+            />
+          </div>
+          <h3 className=" text-3xl text-white font-urbanistBold text-center -mt-5 mb-2">
+            Solo Player
+          </h3>
+        </div>
 
-      <UpdateElectron />
+        <div className=" flex justify-center items-center">
+          <p className=" font-urbanistMd text-xs text-white text-center mx-5">
+            Play your favorite music everyday using the one and only solo player
+            !!
+          </p>
+        </div>
+        <div className=" flex justify-center items-center mt-10">
+          <button
+            style={{ backgroundColor: "rgba(225, 225, 225, 0.2)" }}
+            className=" w-[80%] flex items-center text-sm p-1 border-[1px] outline-none mt-5 shadow-inline border-none rounded text-white text-left font-urbanist"
+          >
+            <FaList className="mx-1" />
+            Playlist
+          </button>
+        </div>
+
+        <div className=" flex justify-center items-center mt-1">
+          <button
+            onClick={() => {
+              console.log("history clicked");
+            }}
+            style={{ backgroundColor: "rgba(225, 225, 225, 0.2)" }}
+            className=" w-[80%] flex items-center  text-sm p-1 border-[1px] outline-none text-left mt-1 shadow-inline border-none rounded text-white font-urbanist"
+          >
+            <FaHistory className="mx-1 text-violet-700" />
+            History
+          </button>
+        </div>
+
+        <div className=" flex justify-center items-center mt-1">
+          <button
+            onClick={() => {
+              console.log("history clicked");
+            }}
+            style={{ backgroundColor: "rgba(225, 225, 225, 0.2)" }}
+            className=" w-[80%] items-center flex text-sm p-1 border-[1px] outline-none text-left mt-1 shadow-inline border-none rounded text-white font-urbanist"
+          >
+            <FaHeart className="mx-1" />
+            Favorite
+          </button>
+        </div>
+
+        <div className=" flex justify-center items-center mt-1">
+          <button
+            onClick={() => {
+              console.log("history clicked");
+            }}
+            style={{ backgroundColor: "rgba(225, 225, 225, 0.2)" }}
+            className=" w-[80%]  text-sm p-1 flex items-center border-[1px] outline-none text-left mt-1 shadow-inline border-none rounded text-white font-urbanist"
+          >
+            <FaArchive className="mx-1" />
+            Archived
+          </button>
+        </div>
+      </div>
+      <div
+        className=" flex-row justify-center items-center h-screen"
+        style={{ flex: 4 }}
+      ></div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        urbanist: ["urbanist", "system-ui"],
+        montserrat: ["montserrat", "sans-serif"],
+        urbanist: ["urbanist", "sans-serif"],
+        urbanistBold: ["urbanistBold", "sans-serif"],
+        urbanistMd: ["urbanistMedium", "sans-serif"],
+      },
+    },
   },
   corePlugins: {
     preflight: false,
   },
   plugins: [],
-}
+};
